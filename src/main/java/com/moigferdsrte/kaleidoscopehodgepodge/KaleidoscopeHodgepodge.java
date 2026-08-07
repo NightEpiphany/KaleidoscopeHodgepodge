ecf31875a8ed9861763c3a89004b644e3d3b4378
yanli <yanli@nb-tec.cn>
2026-08-08 00:57:20 +0800

    Fix build #2

--- src/main/java/com/moigferdsrte/kaleidoscopehodgepodge/KaleidoscopeHodgepodge.java
+++ src/main/java/com/moigferdsrte/kaleidoscopehodgepodge/KaleidoscopeHodgepodge.java
@@ -2,19 +2,15 @@ package com.moigferdsrte.kaleidoscopehodgepodge;
 
 import com.moigferdsrte.kaleidoscopehodgepodge.config.GeneralConfig;
 import fuzs.forgeconfigapiport.fabric.api.v5.ConfigRegistry;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.resources.Identifier;
 
 import net.neoforged.fml.config.ModConfig;
-import org.jetbrains.annotations.Contract;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public final class KaleidoscopeHodgepodge implements ModInitializer, ClientModInitializer {
+public final class KaleidoscopeHodgepodge implements ModInitializer {
 	public static final String MOD_ID = "kaleidoscopehodgepodge";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
@@ -27,11 +23,5 @@ public final class KaleidoscopeHodgepodge implements ModInitializer, ClientModIn
 
 	public static Identifier id(String path) {
 		return Identifier.fromNamespaceAndPath(MOD_ID, path);
-	}
-
-	@Environment(EnvType.CLIENT)
-	@Override
-	public void onInitializeClient() {
-
 	}
 }
---
+++ src/main/java/com/moigferdsrte/kaleidoscopehodgepodge/KaleidoscopeHodgepodgeClient.java
@@ -0,0 +1,13 @@
+package com.moigferdsrte.kaleidoscopehodgepodge;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class KaleidoscopeHodgepodgeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
