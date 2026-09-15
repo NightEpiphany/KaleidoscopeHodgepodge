8e0211df1e9d04eacecc898686e531da81b9b3bd
NightEpiphany <nightepiphany0401@gmail.com>
2026-09-16 01:30:01 +0800

    Update dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,7 @@ dependencies {
 	// To change the versions see the gradle.properties file
 	minecraft("com.mojang:minecraft:${providers.gradleProperty("minecraft_version").get()}")
 	implementation("net.fabricmc:fabric-loader:${providers.gradleProperty("loader_version").get()}")
-	//implementation ("maven.modrinth:kaleidoscope-cookery-refabricated:${providers.gradleProperty("kaleidoscope_cookery_version").get()}-fabric+mc${providers.gradleProperty("minecraft_version").get()}")
-	implementation(files("libs/kaleidoscope_cookery-1.4.1.6-fabric+mc26.3.jar"))
+	implementation ("maven.modrinth:kaleidoscope-cookery-refabricated:${providers.gradleProperty("kaleidoscope_cookery_version").get()}-fabric+mc${providers.gradleProperty("minecraft_version").get()}")
 	implementation("net.fabricmc.fabric-api:fabric-api:${providers.gradleProperty("fabric_api_version").get()}")
 	// Kaleidoscope Cookery declares this as a required runtime dependency.
 	compileOnly("fuzs.forgeconfigapiport:forgeconfigapiport-fabric:${providers.gradleProperty("forge_config_api_version").get()}")
